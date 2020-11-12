6ce6d470ffb5f506f2270b94d8d5d5e3c6e5e85b
Roroji <khatanzorigb@gmail.com>
2020-11-12 21:32:15 +0800

    aaa

--- rosalind/cstr/main.py
+++ rosalind/cstr/main.py
@@ -1,51 +1,17 @@
-def character_table(t):
+def get_character_table(dna_strings):
+    str_collection = []
 
-    if len (t[0]) < 1:
-        return None
-        
-    if len(t) < 2:
-        return None
+    positions = [''.join(map(str, map(int, map(s[0].__eq__, s))))
+                   for s in map(''.join, zip(*filter(None, dna_strings)))]
     
-    char = [[0 for i in t]]
+    for k in set(positions):
+        if sum(map(int, k)) not in (0, 1, len(k), len(k) - 1):
+            str_collection.append(k)
     
-    taxa = [[0], []]
-    
-    for i in range(1, len(t)):
-        if t[i][0] == t[0][0]:
-            taxa[0].append(i)
-        else:
-            taxa[1].append(i)
-            
-    if taxa[1] == []:
-        return None
-    
-    a, b = [[], []]
-    for i in taxa[0]:
-        char[0][i] = 1
-        a.append(t[i][1:])
-    for j in taxa[1]:
-        b.append(t[j][1:])
-    
-    row1 = character_table(a)
-    if row1 != None:
-        for i in row1:
-            char.append(i)
-            
-    row2 = character_table(b)
-    if row2 != None:
-        for j in row2:
-            char.append(j)
-    
-    return char
-    
-    
-def main():
-    with open('./rosalind_cstr.txt', 'r') as infile:
-        strings = infile.read().strip().split('\n')
-    
-    answer = character_table(strings)
-    
-    print('\n'.join([''.join(map(str, answer[i])) for i in range(len(answer))]))
-    
+    return str_collection 
+                 
 if __name__ == '__main__':
-    main()
+    with open('rosalind_cstr.txt') as f:
+        dna_strings = map(str.strip, f.readlines())
+        
+        print ("\n".join(str(x) for x in get_character_table(dna_strings)))
